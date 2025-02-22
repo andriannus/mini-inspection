577e68e1e1ea16e7a00e252ed9fdeeb71c9411ad
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-22 14:06:28 +0700

    feat: setup linter

---
+++ lint-staged.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
+  '*.{mjs,ts,tsx}': (filenames) => {
+    const onOneLine = filenames.join(' ');
+    return `yarn lint ${onOneLine}`;
+  },
+};
+
+export default config;
