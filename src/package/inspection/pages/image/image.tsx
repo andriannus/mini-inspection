180cb86ebcda5320896ba9ad8cb367a7e6943ceb
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-26 15:29:41 +0700

    feat: filter file by date

--- src/package/inspection/pages/image/image.tsx
+++ src/package/inspection/pages/image/image.tsx
@@ -1,11 +1,13 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { Button, Col, Row, Spin } from 'antd';
-import { lazy } from 'react';
+import { Button, Col, DatePicker, Empty, Row, Spin, Typography } from 'antd';
+import type { RangePickerProps } from 'antd/es/date-picker';
+import dayjs from 'dayjs';
+import { lazy, useMemo, useState } from 'react';
 
 import { fetchInspectionFiles } from '~/inspection/apis/upload';
 import { COL_PROPS } from '~/inspection/components/form/constants';
 
-import { ContainerForAction } from './styles';
+import { ContainerForAction, StyledRow } from './styles';
 
 const ImageCard = lazy(() =>
   import('./components/card').then((module) => ({
@@ -14,12 +16,23 @@ const ImageCard = lazy(() =>
 );
 
 function InspectionPageImage() {
+  const [dateRange, setDateRange] = useState<string[]>([]);
+
+  const dateRangeFormatted = useMemo<RangePickerProps['value']>(() => {
+    const [startDate, endDate] = dateRange;
+
+    if (!startDate || !endDate) return [undefined, undefined];
+    return [dayjs(startDate), dayjs(endDate)];
+  }, [dateRange]);
+
   const queryFiles = useInfiniteQuery({
-    queryKey: ['inspection-files'],
+    queryKey: ['inspection-files', dateRange[0], dateRange[1]],
     queryFn: ({ pageParam }) => {
       return fetchInspectionFiles({
         params: {
           page: pageParam,
+          start_date: dateRange[0],
+          end_date: dateRange[1],
         },
       });
     },
@@ -43,12 +56,27 @@ function InspectionPageImage() {
 
   if (queryFiles.isError) return null;
 
-  if (queryFiles.data?.pages[0].pagination.total_page === 0) {
-    return <span>No data</span>;
-  }
-
   return (
     <>
+      <StyledRow gutter={[16, 16]}>
+        <Col sm={24} lg={12} xl={8}>
+          <Typography.Text strong>Filter by date</Typography.Text>
+
+          <DatePicker.RangePicker
+            value={dateRangeFormatted}
+            disabledDate={(current) => current > dayjs()}
+            format="DD MMM YYYY"
+            onChange={(_, dates) => {
+              setDateRange(dates);
+            }}
+          />
+        </Col>
+      </StyledRow>
+
+      {queryFiles.data?.pages[0].pagination.total_page === 0 && (
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+      )}
+
       <Row gutter={[16, 16]}>
         {queryFiles.data?.pages.map((group) => {
           return group.files.map((file) => {
--- src/package/inspection/pages/image/styles.ts
+++ src/package/inspection/pages/image/styles.ts
@@ -1,7 +1,16 @@
+import { Row } from 'antd';
 import styled from 'styled-components';
 
 export const ContainerForAction = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 16px;
+`;
+
+export const StyledRow = styled(Row)`
+  .ant-picker {
+    width: 100%;
+  }
+
+  margin-bottom: 16px;
 `;
